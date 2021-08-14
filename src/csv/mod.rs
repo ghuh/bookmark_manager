235cf9abac07838ca694a0eeedfbd8842f5edd52
kphayen <kphayen@gmail.com>
2021-08-13 19:20:59 -0600

    Move the create CSV method

--- src/csv/mod.rs
+++ src/csv/mod.rs
@@ -1,3 +1,23 @@
 pub mod csv_line_reader;
 
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
+use anyhow::{Result, Context};
+
 pub use csv_line_reader::CsvLineReader;
+use crate::output_utils;
+
+const ORDERED_HEADERS: [&'static str; 3] = ["URL", "DESCRIPTION", "TAGS"];
+
+/// If the CSV already exists, do nothing.  Otherwise create it with headers
+pub fn create_csv(csv_path: &str) -> Result<()> {
+    let path = Path::new(csv_path);
+    if !path.exists() {
+        let mut file = File::create(path).context("Couldn't create CSV file")?;
+        writeln!(file, "{}", ORDERED_HEADERS.join("|")).context("Couldn't write headers to new CSV file")?;
+        output_utils::print_success("CSV file created")
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 
 use std::env;
-use std::path::Path;
-use std::fs::File;
-use std::io::Write;
 use clap::Clap;
-use anyhow::{Result, Context};
+use anyhow::Result;
 
 use config::Command;
 use crate::commands::add::add;
@@ -21,31 +18,18 @@ mod commands;
 mod output_utils;
 
 const ENV_CSV: &str = "BOOKMARK_MANAGER_CSV";
-const ORDERED_HEADERS: [&'static str; 3] = ["URL", "DESCRIPTION", "TAGS"];
 
 fn main() -> Result<()> {
     let opt = config::Opts::parse();
 
     let csv = env::var(ENV_CSV)
         .expect(&*format!("Environmental variable {} must be set", ENV_CSV));
-    create_csv(csv.as_str())?;
+    csv::create_csv(csv.as_str())?;
 
     match opt.cmd {
         Command::Add(add_opts) => add(&add_opts, &csv)?,
         Command::Search(search_opts) => search(&search_opts, &csv)?,
         Command::Tags(tags_opts) => tags(&tags_opts, &csv)?,
-    }
-
-    Ok(())
-}
-
-/// If the CSV already exists, do nothing.  Otherwise create it with headers
-fn create_csv(csv_path: &str) -> Result<()> {
-    let path = Path::new(csv_path);
-    if !path.exists() {
-        let mut file = File::create(path).context("Couldn't create CSV file")?;
-        writeln!(file, "{}", ORDERED_HEADERS.join("|")).context("Couldn't write headers to new CSV file")?;
-        output_utils::print_success("CSV file created")
     }
 
     Ok(())
